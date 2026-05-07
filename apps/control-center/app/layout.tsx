20305d52c77232149fb6426314e2dcb6f00614e3
Ariel Sanchez Rojas <112988517+ArielSanroj@users.noreply.github.com>
2026-05-07 10:58:55 -0500

    chore(layout): remove unconfigured Hotjar script
    Removed Hotjar script integration from layout.

--- apps/control-center/app/layout.tsx
+++ apps/control-center/app/layout.tsx
@@ -193,14 +193,8 @@ export default function RootLayout({
                                       }}
                                     />
                   
-                    {/* Hotjar - replace HOTJAR_SITE_ID with your actual site ID from hotjar.com */}
-                          <Script
-                                      id="hotjar-script"
-                                      strategy="afterInteractive"
-                                      dangerouslySetInnerHTML={{
-                                                    __html: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:HOTJAR_SITE_ID,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-                                      }}
-                                    />
+
+
                   
                           <script
                                       type="application/ld+json"
